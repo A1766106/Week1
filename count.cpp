5c3a58d781be4afa2fc7d7bbed3b2c258111b813
Max Bruce <maxbruce@student-10-201-04-059.wireless.adelaide.edu.au>
2022-07-26 16:49:35 +0930

    cheer

--- count.cpp
+++ count.cpp
@@ -1,1 +1,16 @@
-#incude <iostream>
+#include <iostream>
+
+int count(int array[], int n){
+
+    int even = 0;
+
+    for (int i = 0; i < n; i++){
+    if (array[i]%2 == 0) {
+        even = even + 1;
+
+
+    }}
+  
+    return even;
+}
+    
