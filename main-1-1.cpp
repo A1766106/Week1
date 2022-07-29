56046dda99c1c4f7ec226ac2546baf9f609af2eb
Max Bruce <maxbruce@Maxs-MacBook-Air-2.local>
2022-07-29 18:14:18 +0930

    cheer

--- main-1-1.cpp
+++ main-1-1.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
+#include <stdlib.h>
 
 extern int sum_array(int array[], int n);
-int main()
+int main(int argc,char **argv)
 {
     int array [5] = {4,5,6,7,9};
     std::cout << "The sum of the array is: " << sum_array(array, 5) << std::endl ;
