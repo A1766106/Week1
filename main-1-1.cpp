d4e6eea9f0a30109fe99156b27c138d7d94befdd
Max Bruce <maxbruce@Maxs-MacBook-Air-2.local>
2022-07-29 17:56:02 +0930

    cheer

--- main-1-1.cpp
+++ main-1-1.cpp
@@ -1,14 +1,11 @@
 
 #include <iostream>
-#include <stdlib.h>
-
-using namespace std;
 
 extern int sum_array(int*, int);
 int main()
 {
     int array [5] = {4,5,6,7,9};
-    std::cout << "The sum of the array is: " << sum_array(array, 5) << endl ;
+    std::cout << "The sum of the array is: " << sum_array(array, 5) << std::endl ;
 
 return 0 ;
 }
